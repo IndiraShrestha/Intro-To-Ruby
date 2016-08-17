490dbd184393daacec716a1a9729eed6102d5a63
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-08-16 21:01:10 -0400

    Year born assignment file

---
+++ Year_born.rb
@@ -0,0 +1,14 @@
+def birth_year(name_input, age_input)
+	today = 2016
+	year_date = today - age_input
+	puts name_input.to_s + " was born on " + year_date.to_s
+end	
+
+puts "Please enter your name"
+name = gets.chomp
+puts "Please enter your age"
+age = gets.chomp.to_i
+
+puts birth_year(name, age)
+
+
