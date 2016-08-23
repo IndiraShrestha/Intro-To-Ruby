566580a686c57b75aff19ff592caeb07879b456b
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-08-23 10:35:55 -0400

    Added Mad Lib File

---
+++ mad_lib.rb
@@ -0,0 +1,10 @@
+puts "Please enter a noun"
+noun = gets.chomp.to_s
+
+puts "Please enter a verb"
+verb = gets.chomp.to_s
+
+puts "Please enter a adjective"
+adjective = gets.chomp.to_s
+
+puts "#{noun} is the noun you entered #{verb} is the verb and #{adjective} is the adjective" 
