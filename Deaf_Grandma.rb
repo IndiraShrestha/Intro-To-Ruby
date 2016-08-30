5a5d6ae80ddf4f8946c26a8e28da90b7ee4ac785
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-08-30 13:36:59 -0400

    reset the count to zero

--- Deaf_Grandma.rb
+++ Deaf_Grandma.rb
@@ -1,17 +1,28 @@
 puts "Say something to your grandma"
 # ask user to input what they want to say to their grandma
-user_input = gets.chomp 
-
-while user_input != "BYE"
+user_input = gets.chomp
+# count variable to keep track of the number of bye
+count = 0
+# while loop to 
+while user_input != "BYE" || user_input == "BYE" && count < 3
+	# if statement to check if the user_input not upper case 
 	if user_input != user_input.upcase
+		count = 0
 		puts "HUH?!, SPEAK UP SONNY!"
 		puts "Say something else to your grandma. If you want grandma to hear, shout it out!"
 		user_input = gets.chomp
-	else user_input == user_input.upcase
+	# else if statement to check if the user input is upper case and not BYE	
+	elsif user_input == user_input.upcase && user_input != "BYE"
+		count = 0
 		year = rand(1930..1981)
 		puts "NO, NOT SINCE #{year}"	
-
 		puts "Say something else to your grandma. If not shout 'BYE'"
 		user_input = gets.chomp
-	end			
-end	
+	# else statement to check if user_input is BYE and start counting the number of times after	
+	else user_input == "BYE"
+		# increasing count by 1 to keep track of number of BYE so that it's less than 3
+		count += 1
+		puts "Say something else to your grandma. If not shout 'BYE'"
+		user_input = gets.chomp
+	end	# end of if statement		
+end	# end of while statement
