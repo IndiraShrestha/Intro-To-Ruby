317ecbb768a2c284787365e6e5b0a327ee00fb67
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-09-19 17:30:52 -0400

    changed the delete_suspect method

--- guess_who.rb
+++ guess_who.rb
@@ -8,35 +8,43 @@ class Suspects
     @suspects_list = CSV.read('Suspects_file.csv')
   end  
 
-  def show_list_of_suspects #this should show the first name of the suspects
-    puts @suspects_list
-  end  
+  # def show_list_of_suspects #this should show the first name of the suspects
+  #   puts @suspects_list
+  # end  
 end  # end of Suspects class
  
 class Guess_Game 
   
+  attr_reader :guilty_suspect
+
   def initialize
     @suspects = Suspects.new 
-    guilty_suspect = @suspects.suspects_list.sample #How do i initialize the guilty suspect
+    @guilty_suspect = @suspects.suspects_list.sample #How do i initialize the guilty suspect
   end  
+
+  def show_guilty_suspect
+    puts @guilty_suspect
+  end 
+
+  def show_list_of_suspects
+    puts @suspects.suspects_list
+  end
 
   def delete_not_matched_suspects(attribute) #method to delete the suspects with matched attribute.
       @suspects.suspects_list.each do |list|
-        list.each do |attribute|
-          if list.include?(attribute)
-            @suspects.delete(list)
-          end  
-        end 
+        if !list.include?(attribute)
+          @suspects.suspects_list.delete(list)  
+        end   
       end
   end  
 
   def remaining_suspects
-    suspects_left = @suspects.show_list_of_suspects
+    suspects_left = @suspects.suspects_list
   end  
 
 end # end of Guess_Game class
 
-gender_color = ["MALE", "FEMALE", "BLACK", "WHITE", ]
+gender_color = ["BOY", "GIRL", "BLACK", "WHITE"]
 
 ### ------------------------------------------------------------------------------ ###
 
@@ -44,25 +52,18 @@ gender_color = ["MALE", "FEMALE", "BLACK", "WHITE", ]
 
 guess = 0
 guess_who = Guess_Game.new
+guess_who.show_guilty_suspect
+guess_who.show_list_of_suspects
 puts "Please enter an gender/color attribute?"#ask user to input the attribute 
-while guess <= 3
 
   attribute = gets.chomp
-
-  if gender_color.include?(attribute.upcase)
-    # guess_who.delete_not_matched_suspects(attribute)
-    # puts guess_who.remaining_suspects
-    puts "Did it"
+  if gender_color.include?(attribute.upcase)  #if the attribute matches the suspect then tell the user he is closer or else delete the suspects whoes attributes doesn't match
+    guess_who.delete_not_matched_suspects(attribute)
+    puts guess_who.remaining_suspects
   else   
     puts "Please enter a valid gender or color?"
-    guess -= 1
-    next
   end  
-  
-  #if the attribute matches the suspect then tell the user he is closer or else delete the suspects whoes attributes doesn't match
 
-  guess += 1
-end  
 
 
 
