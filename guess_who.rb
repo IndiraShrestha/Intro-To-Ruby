35c2afac216de47ebf47e927bc72bc09dcd94407
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-09-19 16:42:24 -0400

    made changes in the methods

--- guess_who.rb
+++ guess_who.rb
@@ -1,70 +1,64 @@
 require 'csv'
+### ------------------------------------------------------------------------------------------- ###
 class Suspects
 
+  attr_reader :suspects_list
+
   def initialize
-    @suspects_csv = csv.read('filename.csv')
+    @suspects_list = CSV.read('Suspects_file.csv')
   end  
 
-  #isn't it the same if i just put @suspects_csv but maybe i can format this one....
   def show_list_of_suspects #this should show the first name of the suspects
-    suspects_list = [] #maybe this should be in initialized as i will need this instance variable in other places
-    suspects_csv.each do |list|
-      suspect_name = Suspects[list]
-      suspects_list << suspect_name
-    end  
-    puts suspects_list
+    puts @suspects_list
   end  
-end  
-
-#not sure if i need a subclass called guilty as suspect and guilty share the same attributes and nothing more
-class Guilty < Suspects 
+end  # end of Suspects class
+ 
+class Guess_Game 
+  
   def initialize
-  guilty_suspect = @suspects_csv.rand()
-  end
-end  
-
-class Guess 
-
-  # def initialize (guess_count)
-  #   @guess_count = guess_count
-  # end 
-
-  def match_attribute(attribute) #method to check if the attribute doesn't matches suspect. If it doesn't then delete all the suspects with that attribute
-    Guilty.guilty_suspect.each do |x|
-      if attribute == Guilty.guilty_suspect[x]
-        def delete_suspects_with_attribute(attribute)
-      else
-        puts "You are getting close"    
-      end 
-    end 
+    @suspects = Suspects.new 
+    guilty_suspect = @suspects.suspects_list.sample #How do i initialize the guilty suspect
   end  
 
-
-  def delete_suspects_with_attribute(attribute) #method to delete the suspects with matched attribute.
-    Suspect.suspects_list.each do 
-
+  def delete_not_matched_suspects(attribute) #method to delete the suspects with matched attribute.
+      @suspects.suspects_list.each do |list|
+        list.each do |attribute|
+          if list.include?(attribute)
+            @suspects.delete(list)
+          end  
+        end 
+      end
   end  
 
-end  
+  def remaining_suspects
+    suspects_left = @suspects.show_list_of_suspects
+  end  
 
-pool_of_suspects = Suspects.new 
-Suspects.show_list_of_suspects
+end # end of Guess_Game class
 
+gender_color = ["MALE", "FEMALE", "BLACK", "WHITE", ]
 
-# guilty_suspect = Guilty.rand() #this needs to be selected randomly from the pool of suspects and since guilty_suspect and Suspect kind of share same attributes we could sub class guilty_suspect class
+### ------------------------------------------------------------------------------ ###
 
-
-puts "Please enter an attribute?"
-attribute = gets.chomp
-#have a guess object/class which carries the methods match attribute and delete suspect from the suspects_list attribute
-Guess.match_attribute(attribute)
-
-
+# #have a guess object/class which carries the methods match attribute and delete suspect from the suspects_list attribute
 
 guess = 0
-
+guess_who = Guess_Game.new
+puts "Please enter an gender/color attribute?"#ask user to input the attribute 
 while guess <= 3
-  #ask user to input the attribute 
+
+  attribute = gets.chomp
+
+  if gender_color.include?(attribute.upcase)
+    # guess_who.delete_not_matched_suspects(attribute)
+    # puts guess_who.remaining_suspects
+    puts "Did it"
+  else   
+    puts "Please enter a valid gender or color?"
+    guess -= 1
+    next
+  end  
+  
   #if the attribute matches the suspect then tell the user he is closer or else delete the suspects whoes attributes doesn't match
 
   guess += 1
