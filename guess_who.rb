3ee092c687048ba5d4d27973cedd64096a12017c
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-09-19 18:20:46 -0400

    finally delete_suspect working

--- guess_who.rb
+++ guess_who.rb
@@ -26,16 +26,32 @@ class Guess_Game
     puts @guilty_suspect
   end 
 
+  def guilty_suspect_gender
+    gender = @guilty_suspect[1].upcase
+  end  
+
+  def guilty_suspect_color
+    color = @guilty_suspect[2].upcase
+  end 
+
+
   def show_list_of_suspects
     puts @suspects.suspects_list
   end
 
   def delete_not_matched_suspects(attribute) #method to delete the suspects with matched attribute.
-      @suspects.suspects_list.each do |list|
-        if !list.include?(attribute)
-          @suspects.suspects_list.delete(list)  
-        end   
-      end
+      # @suspects.suspects_list.each do |list|
+      #   if list[2].include?(attribute)
+      #     @suspects.suspects_list.delete(list)
+      #   elsif list[1].include?(attribute)
+      #     @suspects.suspects_list.delete(list)  
+      #   else 
+      #     puts "---"  
+      #   end   
+      # end
+      @suspects.suspects_list.delete_if do |list|
+        list[1].include?(attribute) || list[2].include?(attribute)
+      end  
   end  
 
   def remaining_suspects
@@ -44,8 +60,6 @@ class Guess_Game
 
 end # end of Guess_Game class
 
-gender_color = ["BOY", "GIRL", "BLACK", "WHITE"]
-
 ### ------------------------------------------------------------------------------ ###
 
 # #have a guess object/class which carries the methods match attribute and delete suspect from the suspects_list attribute
@@ -53,17 +67,19 @@ gender_color = ["BOY", "GIRL", "BLACK", "WHITE"]
 guess = 0
 guess_who = Guess_Game.new
 guess_who.show_guilty_suspect
-guess_who.show_list_of_suspects
+# guess_who.show_list_of_suspects
 puts "Please enter an gender/color attribute?"#ask user to input the attribute 
 
+while guess <= 3
   attribute = gets.chomp
-  if gender_color.include?(attribute.upcase)  #if the attribute matches the suspect then tell the user he is closer or else delete the suspects whoes attributes doesn't match
+  if attribute.upcase != guess_who.guilty_suspect_color || attribute.upcase != guess_who.guilty_suspect_gender  #if the attribute matches the suspect then tell the user he is closer or else delete the suspects whoes attributes doesn't match
     guess_who.delete_not_matched_suspects(attribute)
     puts guess_who.remaining_suspects
   else   
     puts "Please enter a valid gender or color?"
   end  
-
+  guess += 1
+end
 
 
 
