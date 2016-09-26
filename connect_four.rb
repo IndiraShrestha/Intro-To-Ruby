9e35415f3539adf2600100d9e8e0ff11494cd792
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-09-26 17:53:33 -0400

    updated the method

--- connect_four.rb
+++ connect_four.rb
@@ -1,48 +1,48 @@
 require 'pry'
 class Board
-  attr_accessor :board, :input_squares_array
+  attr_accessor :board
   
-  def initialize 
+  def initialize (row, column)
     @board = []
-    @board << ['X',2,3,4,5,6]
-    @board << [7,8,9,10,11,12]
-    @board << [13,14,15,16,17,18]
-    @board << [19,20,21,22,23,24]
-    @board << [25,26,27,28,29,30]
-    @board << [31,32,33,34,35,36]
-    @board << [37,38,39,40,41,42]
-    @input_squares_array = []
+    @rows = row
+    @columns = column
   end  
 
   def display_board
     # system 'clear'
-    @board.each do |row|
+    @rows.times do |row|
+      row = []
+      @columns.times do |column|
+        row << '_'
+      end 
       puts row.join('|')
-      puts "___________________"
+      @board << row 
     end  
+    # @board.each do |row|
+    #   puts row.join('|')
+    #   puts "___________________"
+    # end  
   end  
 
   def updated_board(column, player)
+    puts @board.length
+    puts @board[0]
+
     if column == 1 
-      if @board[6][(column-1)].is_a?(Numeric) 
-        @board[6][(column-1)] = player  
-      elsif @board[5][(column-1)].is_a?(Numeric)
-        @board[5][(column-1)] = player
-      elsif @board[4][(column-1)].is_a?(Numeric)
-        @board[4][(column-1)] = player 
-      elsif @board[3][(column-1)].is_a?(Numeric)
-        @board[3][(column-1)] = player  
-      elsif @board[2][(column-1)].is_a?(Numeric)
-        @board[2][(column-1)] = player  
-      elsif @board[1][(column-1)].is_a?(Numeric)
-        @board[1][(column-1)] = player  
-      elsif @board[0][(column-1)].is_a?(Numeric)
-        @board[0][(column-1)] = player  
-      elsif 
-        puts "Please select a valid column"          
-      end 
+      @board.each do |row|
+        row.each_index do |column|
+          stack = @board.length - 1 - column
+          if @board[stack][(column-1)] != '_'
+            puts "The column is full"
+          elsif @board[stack][(column-1)] == '_'      
+            @board[stack][(column-1)] = player
+          end  
+        end
+      end  
+      
+
     elsif column == 2
-      if @board[6][(column-1)].is_a?(Numeric) 
+      if @board[6][(column-1)] == '_'
         @board[6][(column-1)] = player  
       elsif @board[5][(column-1)].is_a?(Numeric)
         @board[5][(column-1)] = player
@@ -57,19 +57,16 @@ class Board
       elsif @board[0][(column-1)].is_a?(Numeric)
         @board[0][(column-1)] = player  
       elsif 
-        puts "Please select a valid column"          
+        puts "The column is full. Please select another column"          
       end 
 
     end  
   end  
 
-  def check_vertical_input(square)
-
-
-  end  
+  
 end
 
-board = Board.new
+board = Board.new(6,7)
 
 board.display_board  
 
