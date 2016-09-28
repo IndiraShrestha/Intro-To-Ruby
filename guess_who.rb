6816d37781ddc6ae652cf28e5792349473e7099c
Indira Shresthagit config --global user.emailindirashrestha.21@gmail.com <indirashrestha.21@gmail.com>
2016-09-28 15:08:15 -0400

    redid the guess_who

--- guess_who.rb
+++ guess_who.rb
@@ -1,85 +1,72 @@
 require 'csv'
+require 'terminal-table'
+require 'pry'
 ### ------------------------------------------------------------------------------------------- ###
 class Suspects
 
-  attr_reader :suspects_list
+  attr_reader :suspects_list, :guilty_suspect
 
   def initialize
-    @suspects_list = CSV.read('Suspects_file.csv')
-  end  
-
-  # def show_list_of_suspects #this should show the first name of the suspects
-  #   puts @suspects_list
-  # end  
-end  # end of Suspects class
- 
-class Guess_Game 
-  
-  attr_reader :guilty_suspect
-
-  def initialize
-    @suspects = Suspects.new 
-    @guilty_suspect = @suspects.suspects_list.sample #How do i initialize the guilty suspect
-  end  
-
-  def show_guilty_suspect
-    puts @guilty_suspect
+    @suspects_list = CSV.read('Suspects_file.csv')  
+    @remaining_suspects = []
+    @guilty_suspect = [] 
   end 
 
-  def guilty_suspect_gender
-    gender = @guilty_suspect[1].upcase
-  end  
-
-  def guilty_suspect_color
-    color = @guilty_suspect[2].upcase
+  def guilty_suspect
+    @guilty_suspect = @suspects_list.sample
   end 
-
 
   def show_list_of_suspects
-    puts @suspects.suspects_list
+    @suspects_list
+    table = Terminal::Table.new :title =>'Suspects', :headings =>['Name', 'Gender', 'Color', 'Hair', 'EyeColor'], :rows => @suspects_list 
+    puts table
   end
 
-  def delete_not_matched_suspects(attribute) #method to delete the suspects with matched attribute.
-      # @suspects.suspects_list.each do |list|
-      #   if list[2].include?(attribute)
-      #     @suspects.suspects_list.delete(list)
-      #   elsif list[1].include?(attribute)
-      #     @suspects.suspects_list.delete(list)  
-      #   else 
-      #     puts "---"  
-      #   end   
-      # end
-      @suspects.suspects_list.delete_if do |list|
-        list[1].include?(attribute) || list[2].include?(attribute)
-      end  
+  def delete_not_matched_gender(gender) #method to delete the suspects with matched attribute.
+    @suspects_list.each do |suspects|
+      puts suspects[1]
+      if suspects[1] == gender && suspects[1] != @guilty_suspect[1]
+        index_of_suspect = @suspects_list.index(suspects)
+        @suspects_list.delete_at(index_of_suspect)
+      end
+    end
+    binding.pry
   end  
-
-  def remaining_suspects
-    suspects_left = @suspects.suspects_list
-  end  
-
-end # end of Guess_Game class
+ 
+end  # end of Suspects class
 
 ### ------------------------------------------------------------------------------ ###
 
-# #have a guess object/class which carries the methods match attribute and delete suspect from the suspects_list attribute
-
 guess = 0
-guess_who = Guess_Game.new
-guess_who.show_guilty_suspect
-# guess_who.show_list_of_suspects
-puts "Please enter an gender/color attribute?"#ask user to input the attribute 
+guess_who = Suspects.new
 
-while guess <= 3
-  attribute = gets.chomp
-  if attribute.upcase != guess_who.guilty_suspect_color || attribute.upcase != guess_who.guilty_suspect_gender  #if the attribute matches the suspect then tell the user he is closer or else delete the suspects whoes attributes doesn't match
-    guess_who.delete_not_matched_suspects(attribute)
-    puts guess_who.remaining_suspects
-  else   
-    puts "Please enter a valid gender or color?"
-  end  
-  guess += 1
+suspect_name = guess_who.guilty_suspect
+puts suspect_name
+puts guess_who.show_list_of_suspects # guess_who.show_list_of_suspects
+puts "Please choose an attribute of 1 for Gender, 2 for Skin Color, 3 for Hair color, 4 for Eye color?" #ask user to input the attribute 
+attribute = gets.chomp.to_i
+
+if attribute == 1
+  puts "Please enter the Gender(Boy or Girl)?"
+  gender = gets.chomp.to_s.downcase
+elsif attribute == 2
+  puts "Please enter the Skin Color(Black or White)?"
+  skin_color = gets.chomp.to_s.downcase
+elsif attribute == 3
+  puts "Please enter the Hair Color(Auburn, Black or Brown or Blonde)?"
+  hair_color = gets.chomp.to_s.downcase
+elsif attribute == 4
+  puts "Please enter the Eye Color(Green, Brown, Blue)?"
+  eye_color = gets.chomp.to_s.downcase  
+else
+  puts "Please enter a valid choice"
 end
+
+guess_who.delete_not_matched_gender(gender)
+guess_who.show_list_of_suspects
+binding.pry
+
+
 
 
 
